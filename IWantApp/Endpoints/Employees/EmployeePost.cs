b36f9b7458ef1e1c0c51f3cfdda71b92f5236c2d
Ju Couto <jnalmeida@ymail.com>
2022-12-20 14:50:09 -0300

    Adicionando Migrations e employeePost

---
+++ IWantApp/Endpoints/Employees/EmployeePost.cs
@@ -0,0 +1,35 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace IWantApp.Endpoints.Employees;
+
+public class EmployeePost
+{
+    public static string Template => "/employee";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(EmployeeRequest employeeRequest, UserManager<IdentityUser> userManager)
+    {
+
+        var user = new IdentityUser { UserName = employeeRequest.Email, Email = employeeRequest.Email };
+        var result = userManager.CreateAsync(user, employeeRequest.Password).Result;
+
+        if (!result.Succeeded)
+            return Results.BadRequest(result.Errors.First());
+
+        var claimResult= userManager.AddClaimAsync(user, new Claim("EmplyeeCode", employeeRequest.EmployeeCode)).Result;
+
+        if (!claimResult.Succeeded)
+            return Results.BadRequest(result.Errors.First());
+
+        claimResult = userManager.AddClaimAsync(user, new Claim("Name", employeeRequest.Name)).Result;
+
+        if (!claimResult.Succeeded)
+            return Results.BadRequest(result.Errors.First());
+
+        return Results.Created($"/categories/{user.Id}", user.Id); // Após realizar o post, irá mostrar apenas o id que foi criado .
+    }
+}
--- IWantApp/Program.cs
+++ IWantApp/Program.cs
@@ -1,10 +1,18 @@
 using IWantApp.Endpoints.Categories;
+using IWantApp.Endpoints.Employees;
 using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddNpgsql<ApplicationDbContext>(builder.Configuration["ConnectionString:IWantDb"]);
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequiredLength = 3;
+})
     .AddEntityFrameworkStores<ApplicationDbContext>(); // Adicionando o identity como serviço.
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true); // configuração para BD postgres reconhecer A DATA LOCAL.
 
@@ -30,5 +38,7 @@ app.UseHttpsRedirection();
 app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Handle);
 app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.Handle);
 app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
+app.MapMethods(EmployeePost.Template, EmployeePost.Methods, EmployeePost.Handle);
+
 
 app.Run();
