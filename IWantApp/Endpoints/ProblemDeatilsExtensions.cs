7eeb391a1d52fe1d20636053b2a063a5833a7f9f
Ju Couto <jnalmeida@ymail.com>
2022-12-20 16:47:34 -0300

    Acrescentando paginação

--- IWantApp/Endpoints/ProblemDeatilsExtensions.cs
+++ IWantApp/Endpoints/ProblemDeatilsExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Flunt.Notifications;
+using Microsoft.AspNetCore.Identity;
 
 namespace IWantApp.Endpoints;
 
@@ -11,5 +12,18 @@ public static class ProblemDeatilsExtensions
             .GroupBy(g => g.Key)
             .ToDictionary(g => g.Key, g => g.Select(x => x.Message).ToArray());
        
+    }
+
+    public static Dictionary<string, string[]> ConvertToProblemDetails(this IEnumerable<IdentityError> error)
+    {
+        // forma 1 para tratar erros, somente com e description.
+        //return error
+        //    .GroupBy(g => g.Code)
+        //    .ToDictionary(g => g.Key, g => g.Select(x => x.Description).ToArray());
+
+        // Forma 2 para tratar erros.
+        var dictionary = new Dictionary<string, string[]>();
+        dictionary.Add("Error", error.Select(e => e.Description).ToArray());
+        return dictionary;
     }
 }
