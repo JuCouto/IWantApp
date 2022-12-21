ab8d4415f5a142c724c48831537311af4f459af7
Ju Couto <jnalmeida@ymail.com>
2022-12-20 22:01:17 -0300

    Separando o método query/Dapper em uma classe de infra

---
+++ IWantApp/Infra/Data/QueryAllUsersWithClaimName.cs
@@ -0,0 +1,24 @@
+﻿namespace IWantApp.Infra.Data;
+
+public class QueryAllUsersWithClaimName
+{
+	private readonly IConfiguration configuration;
+
+	public QueryAllUsersWithClaimName(IConfiguration configuration)
+	{
+		this.configuration = configuration;
+	}
+
+    // Método com DAPPER (não está fazendo a pesquisa)
+    //public IEnumerable<EmployeeResponse> Execute (int page, int? rows)
+    //{
+
+    //    var db = new NpgsqlConnection(configuration["ConnectionString:IWantDb"]);
+    //    string query = @"select 'Email', 'ClaimValue' as Name from AspNetUsers anu INNER JOIN AspNetUserClaims anuc ON anu.Id = anuc.UserId and ClaimType = 'Name' order by name OFFSET(@page - 1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
+
+    //    var employees = db.Query<EmployeeResponse>(query, new {page, rows}
+    //        );
+
+    //    return Results.Ok(employees);
+    //}
+}
